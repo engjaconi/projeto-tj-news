91a4e93fca63712d828d164e7fabedf27d80d487
engjaconi <118689946+engjaconi@users.noreply.github.com>
2023-01-12 07:44:31 -0300

    Ajuste para evitar reload da página desnecessários

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,15 @@
 let navList = document.getElementById('nav__list');
-
 let menuIcon = document.getElementById('icon__menu');
+let sizePage = window.innerWidth;
 
 function resizePage() {
-    if (window.innerWidth >= 768) {
+    let newSizePage = window.innerWidth;
+
+    if (newSizePage >= 767 && sizePage < 767) {
         navList.style.display = 'block';
         menuIcon.style.display = 'none';
         document.location.reload(true);
-    } else {
+    } else if(newSizePage <= 766 && sizePage >= 767){
         navList.style.display = 'none';
         menuIcon.style.display = 'block';
         document.location.reload(true);
